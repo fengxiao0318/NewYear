75ad2d9e41c68d26efc04226f218c048afa8193b
fengxiao0318 <122847872+fengxiao0318@users.noreply.github.com>
2023-01-20 16:16:34 +0800

    Update letter.js

--- langmanxinfeng/static/js/letter.js
+++ langmanxinfeng/static/js/letter.js
@@ -7,7 +7,7 @@ $("#open").click(function(){
 	{
 		var typed = new Typed('.letter', {
 			strings: ["^1000Ms&nbsp;&nbsp;mei", 
-				"M^200ei<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;初次相识于2023 . 1 . 14，何其幸运，2023年的第一场雪是和你一起度过短暂美好时光<br>很高兴能够认识到你这样一位不错的女孩儿！<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2023，新春快乐！天天开心、没有烦恼！<br><br><p style='float:right; display:block; width:80px;'>^1000feng^200xiao</p>"],
+				"M^200ei<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;初次相识于2023 . 1 . 14，何其幸运，2023年的第一场雪是和你一起度过短暂美好时光<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;很高兴能够认识到你这样一位不错的女孩儿！<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2023，新春快乐！天天开心、没有烦恼！<br><br><p style='float:right; display:block; width:80px;'>^1000feng^200xiao</p>"],
 			typeSpeed: 100,
 			backSpeed: 50
 		});
